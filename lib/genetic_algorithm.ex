e388d5c68a211331fda1dde4c2cccae84f1d0b86
basvanwesting <basvanwesting@hotmail.com>
2022-09-15 16:32:49 +0200

    serialize and deserialize with Jason/Serde

--- lib/genetic_algorithm.ex
+++ lib/genetic_algorithm.ex
@@ -3,4 +3,6 @@ defmodule GeneticAlgorithm do
 
   # When your NIF is loaded, it will override this function.
   def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  def point(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  def parse(_a), do: :erlang.nif_error(:nif_not_loaded)
 end
--- native/genetic_algorithm/src/lib.rs
+++ native/genetic_algorithm/src/lib.rs
@@ -3,4 +3,44 @@ fn add(a: i64, b: i64) -> i64 {
     a + b
 }
 
-rustler::init!("Elixir.GeneticAlgorithm", [add]);
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: i64,
+    y: i64,
+}
+
+#[rustler::nif]
+fn point(a: i64, b: i64) -> () {
+    let point = Point { x: a, y: b };
+
+    // Convert the Point to a JSON string.
+    let serialized = serde_json::to_string(&point).unwrap();
+
+    // Prints serialized = {"x":1,"y":2}
+    println!("serialized = {}", serialized);
+
+    // Convert the JSON string back to a Point.
+    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
+
+    // Prints deserialized = Point { x: 1, y: 2 }
+    println!("deserialized = {:?}", deserialized);
+}
+
+#[rustler::nif]
+fn parse(input: String) -> String {
+    // Convert the JSON string back to a Point.
+    let mut deserialized: Point = serde_json::from_str(&input).unwrap();
+
+    // Prints deserialized = Point { x: 1, y: 2 }
+    println!("deserialized = {:?}", deserialized);
+
+    deserialized.y = 10;
+
+    // Convert the Point to a JSON string.
+    let serialized = serde_json::to_string(&deserialized).unwrap();
+    serialized
+}
+
+rustler::init!("Elixir.GeneticAlgorithm", [add, point, parse]);
