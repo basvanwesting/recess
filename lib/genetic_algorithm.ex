784cd609061fecbae6fdd18e8a58ad825c5577af
basvanwesting <basvanwesting@hotmail.com>
2022-09-15 16:17:54 +0200

    Add rustler

---
+++ lib/genetic_algorithm.ex
@@ -0,0 +1,6 @@
+defmodule GeneticAlgorithm do
+  use Rustler, otp_app: :recess, crate: "genetic_algorithm"
+
+  # When your NIF is loaded, it will override this function.
+  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+end
